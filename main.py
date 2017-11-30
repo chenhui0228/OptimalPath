816db86610214087ee63503f515f412ad2fdc3e5
mit6300 <zhao6300@gmail.com>
2017-11-30 23:31:06 +0800

    change

--- main.py
+++ main.py
@@ -144,13 +144,26 @@ class MailMan(object):
             self.x = x
             self.y = y
 
+def test():
+    a = PackageMap()
+    walls = ((0, 5), (1, 0), (1, 1), (1, 5), (2, 3),
+             (3, 1), (3, 2), (3, 5), (4, 1), (4, 4), (5, 1))
+    a.init(6, 6, walls)
+    path = a.find_path((0, 0), (5, 5))
+    path2 = a.find_path((5, 5), (0, 0))
+    print path
+    print path2
+    man = MailMan()
+    man.go(path[0])
+
 if __name__ == '__main__':
     mailman = MailMan()
     mailman.init()
     packagemap = PackageMap()
     packagemap.init(12, 12, mailman.blocks)
-    path = packagemap.find_path((0, 0), (0, 0))
-    print path
+
+    test()
+
 
 
 
