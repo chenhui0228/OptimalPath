a97fef1a29b9cf48066f07bdacdb8d0b1fd11472
chenhui0228 <648966459@qq.com>
2017-12-01 11:24:52 +0800

    add map

--- map.py
+++ map.py
@@ -6,15 +6,15 @@ BASE_URL = 'http://10.2.5.64/test'
 
 class RequestAPI:
     def __init__(self):
-        pass
+        self.headers = {'Content-Type': 'application/json'}
 
     def initialize(self, url, data=None):
-        res = requests.post(url, data=data)
-        return json.load(res.text)
+        res = requests.post(url, data=data, headers=self.headers)
+        return json.loads(res.text)
 
     def go(self, url, data):
-        res = requests.post(url, data=data)
-        return json.load(res.text)
+        res = requests.post(url, data=data,headers=self.headers)
+        return json.loads(res.text)
 
 
 class Map:
@@ -33,16 +33,16 @@ class Map:
 
     def __genarate_map(self):
         data = {'name': 'chenhui'}
-        ret = self.request.initialize('http://10.2.5.64/test')
-        if ret.msg == 'OK':
-            self.ai = (ret.state.ai.x, ret.state.ai.y)
-            for w in ret.state.walls:
-                self.walls.append((w.x, w.y))
-            for p in ret.state.jobs:
-                self.__packages = [(p.x, p.y)]
-                self.packages.append((p.x, p.y, p.value))
-            self.score = ret.state.score
-            self.id = ret.id
+        ret = self.request.initialize('http://10.2.5.64/test', data=data)
+        if ret['msg'] == 'OK':
+            self.ai = (ret['state']['ai']['x'], ret['state']['ai']['y'])
+            for w in ret['state']['walls']:
+                self.walls.append((w['x'], w['y']))
+            for p in ret['state']['jobs']:
+                self.__packages.append((p['x'], p['y']))
+                self.packages.append((p['x'], p['y'], p['value']))
+            self.score = ret['state']['score']
+            self.id = ret['id']
 
     def go(self, direction):
         url = 'http://10.2.5.64/test/' + self.id + '/move'
@@ -50,16 +50,16 @@ class Map:
         __packages_now = []
         packages_now = []
         ret = self.request.go(url, data)
-        if ret.msg == 'OK':
-            self.ai = (ret.state.ai.x, ret.state.ai.y)
-            for p in ret.state.jobs:
-                __packages_now.append((p.x, p.y))
-                packages_now.append((p.x, p.y, p.value))
-            self.score = self.score + ret.reward
+        if ret['msg'] == 'OK':
+            self.ai = (ret['state']['ai']['x'], ret['state']['ai']['y'])
+            for p in ret['state']['jobs']:
+                __packages_now.append((p['x'], p['y']))
+                packages_now.append((p['x'], p['y'], p['value']))
+            self.score = self.score + ret['reward']
             if cmp(self.__packages.sort(), __packages_now.sort()) != 0:
                 self.__packages = __packages_now
                 self.packages = packages_now
                 self.package_newest = True
             else:
                 self.package_newest = False
-            self.end = ret.done
+            self.end = ret['done']
