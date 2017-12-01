3369d5e545b54aae60c1ff7134a09a7c6d66bc67
chenhui0228 <648966459@qq.com>
2017-12-01 13:53:13 +0800

    update map

--- map.py
+++ map.py
@@ -1,9 +1,16 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 import requests
 import json
+
 
 BASE_URL = 'http://10.2.5.64/test'
 
 
+'''
+定义HTTP请求统一接口
+'''
 class RequestAPI:
     def __init__(self):
         self.headers = {'Content-Type': 'application/json'}
@@ -13,12 +20,15 @@ class RequestAPI:
         return json.loads(res.text)
 
     def go(self, url, data):
-        res = requests.post(url, data=data,headers=self.headers)
+        res = requests.post(url, data=data, headers=self.headers)
         return json.loads(res.text)
 
 
+'''
+定义地图，包含ai小哥位置，包裹位置，障碍物位置等信息
+'''
 class Map:
-    def __init__(self):
+    def __init__(self, name):
         self.request = RequestAPI()
         self.ai = None
         self.walls = []
@@ -29,11 +39,14 @@ class Map:
         self.end = False
         self.package_newest = False
         # self.direction = ''
-        self.__genarate_map()
+        self.__genarate_map(name)
 
-    def __genarate_map(self):
-        data = {'name': 'chenhui'}
-        ret = self.request.initialize('http://10.2.5.64/test', data=data)
+    '''
+    初始化地图
+    '''
+    def __genarate_map(self, name):
+        data = {'name': name}
+        ret = self.request.initialize(BASE_URL, data=json.dumps(data))
         if ret['msg'] == 'OK':
             self.ai = (ret['state']['ai']['x'], ret['state']['ai']['y'])
             for w in ret['state']['walls']:
@@ -44,12 +57,15 @@ class Map:
             self.score = ret['state']['score']
             self.id = ret['id']
 
+    '''
+    每走一步，返回分数和更新地图
+    '''
     def go(self, direction):
-        url = 'http://10.2.5.64/test/' + self.id + '/move'
+        url = BASE_URL + '/' + self.id + '/move'
         data = {'direction': direction}
         __packages_now = []
         packages_now = []
-        ret = self.request.go(url, data)
+        ret = self.request.go(url, data=json.dumps(data))
         if ret['msg'] == 'OK':
             self.ai = (ret['state']['ai']['x'], ret['state']['ai']['y'])
             for p in ret['state']['jobs']:
